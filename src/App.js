b85a3126f02ed240cb4fc13277bc4bcd24b5706a
Cyd Villavicencio <villacv@gmail.com>
2021-11-15 17:41:09 -0500

    added searchbox functionality & gradient background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import CardList from "./CardList"
+import { Component } from "react";
+import CardList from "./CardList";
 import { robots } from "./robots";
-import SearchBox from "./SearchBox"
+import SearchBox from "./SearchBox";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
 
-const App = () => {
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
     return (
-        <div>
+      <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
-        </div>
-    )
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
